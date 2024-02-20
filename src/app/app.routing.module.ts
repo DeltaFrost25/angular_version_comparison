08a897dc8e116248d6e3d5ae96779b788f2d1298
DeltaFrost25 <63409989+DeltaFrost25@users.noreply.github.com>
2024-02-20 20:23:32 +0000

    Add lazy loading

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -7,23 +7,23 @@ import { HomeModule } from './modules/home/home.module';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => HomeModule,
-    // loadChildren: () =>
-    //   import('./modules/home/home.module').then((m) => m.HomeModule),
+    //loadChildren: () => HomeModule,
+    loadChildren: () =>
+      import('./modules/home/home.module').then((m) => m.HomeModule),
   },
   {
     path: 'chart',
-    loadChildren: () => ChartPageModule,
-    // loadChildren: () =>
-    //   import('./modules/chart/chart.module').then((m) => m.ChartPageModule),
+    //loadChildren: () => ChartPageModule,
+    loadChildren: () =>
+     import('./modules/chart/chart.module').then((m) => m.ChartPageModule),
   },
   {
     path: 'table',
-    loadChildren: () => TablePageModule,
-    // loadChildren: () =>
-    //   import('./modules/table/table-page.module').then(
-    //     (m) => m.TablePageModule
-    //   ),
+    //loadChildren: () => TablePageModule,
+    loadChildren: () =>
+      import('./modules/table/table-page.module').then(
+        (m) => m.TablePageModule
+      ),
   },
   { path: '**', redirectTo: 'home' },
 ];
